5b57ff9450d9baf647a444752dc6aa3caa3aa5b9
Archisman Datta <mpd19i017@iiitdm.ac.in>
2024-02-22 13:26:42 +0530

    .map()

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(3+4);
+
+// return only names of students in caps
+
+let students = [
+    {name: "Piyush", roll: 31, marks: 80},
+    {name: "Jenny", roll: 15, marks: 69},
+    {name: "Kaushal", roll: 16, marks: 35},
+    {name: "Dilpreet", roll: 7, marks: 55},
+    
+]
+
+const student = students.map((stud) => {return (stud.name)});
+const studentNameInCaps = student.map((item)=>{return item.toUpperCase()});
+console.log(studentNameInCaps);
